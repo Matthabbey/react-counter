39d6c547faeac5c1e413c2f5b5a7b0bda58f4edb
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2022-12-24 08:26:56 +0100

    counter mapping

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,26 +2,26 @@ import React, { Component, useState } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0,
+        value: this.props.value,
         imageUrl: "https://picsum.photos/200",
         tags: ['tag1', 'tag2', 'tag3', 'tag4']
     }
  
 handleIncrement = ()=>{
-    this.setState({count: this.state.count + 1})
+    this.setState({value: this.state.value + 1})
     // console.log("I am clicked now", this)
 }
 handleDecrement = () =>{
-    this.setState({count: this.state.count - 1 })
+    this.setState({value: this.state.value - 1 })
 }
 
 formatCount(){
-    const {count} = this.state
-    return count === 0 ? "Zero" : count
+    const {value} = this.state
+    return value === 0 ? "Zero" : value
 }
 getBadgeClasses(){
     let classes = "badge m-2 badge-"
-  return  classes += this.state.count === 0 ? "warning" : "primary";
+  return  classes += this.state.value === 0 ? "warning" : "primary";
 }
 renderTags(){
     if (this.state.tags.length === 0) return <p>There are no tags available!!</p>
@@ -29,10 +29,11 @@ renderTags(){
 }
 
   render() {
+
    
     return (
       <React.Fragment>
-        {/* <img src={this.state.imageUrl} alt=""/> */}
+        <img src={this.state.imageUrl} alt=""/>
         <h1 className={this.getBadgeClasses()}>{this.formatCount()}</h1>
         <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm m-2">Increment</button>
         <button onClick={this.handleDecrement} className="btn btn-secondary btn-sm m-2">Decrement</button>
